7d55d40ce738ed519c839c9fb9447e6dfd65e2e6
Ld. Nelson <67654295+MwangiNelson@users.noreply.github.com>
2022-03-31 11:58:12 +0300

    commit

---
+++ src/components/body.jsx
@@ -0,0 +1,95 @@
+import React, { Component } from 'react';
+import '../body.css'
+import { Button, TextField } from '@mui/material';
+import { Input } from '@mui/material';
+import { SearchRounded ,ArrowForward, ArrowDownwardRounded} from '@material-ui/icons';
+
+
+class Body
+    extends Component {
+    state = {}
+
+    styles = {
+        paddingTop:'100px',
+        backgroundImage: 'url(/rd_car.jpg)',
+        backgroundSize: 'cover',
+        textAlign: 'left',
+        height: 'max-content'
+    }
+    
+
+    copts = [
+        {
+            text:'Hello'
+        },
+        {
+            text:'Hello'
+        },
+        {
+            text:'Hello'
+        }
+    ]
+
+
+
+
+
+    render() {
+        return (
+
+            <div className="main-body w-100" style={this.styles}>
+
+                <div className="gap"></div>
+
+
+                <div className="container body" style={{ padding: '5rem' }}>
+                    <h2>
+                        FIND THE ENGINE THAT WILL TAKE YOU TO YOUR DESTINATION IN STYLE.
+                    </h2>
+                </div>
+
+                <div className="justify-content-center m-4 p-4" style={{ textAlign: 'center' }}>
+                    <Button
+                        variant='contained'
+                        color='error'
+                        className='centerBtn'
+
+                    >RENT A RIDE.</Button>
+                </div>
+
+                <div className="input-group w-100 justify-content-center p-4" style={{ textAlign: 'center' }}>
+                    <TextField
+                        color='error'
+                        variant='outlined'
+                        sx={{borderColor:'red', input: { color: 'white' }}}
+                        label='Find your ride'
+                    />
+                    <Button
+                        variant='contained'
+                        color='error'
+                    >
+                        <SearchRounded />
+                    </Button>
+
+
+                </div>
+
+
+                <div className="explore justify-content-center w-100">
+                    
+                        <h3 style={{fontSize:'20sp'}} className="link2 w-100 p-2">Explore</h3>
+                        <ArrowDownwardRounded   />
+
+                    
+                </div>
+            </div>
+
+
+
+
+        );
+    }
+}
+
+export default Body
+    ;
